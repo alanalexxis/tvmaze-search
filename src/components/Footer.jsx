d3dbadefaf2811f6c0315088d069a3002cdd4a19
“alanalexxis” <alanalexxis3@gmail.com>
2024-01-28 17:10:03 -0600

    agregado github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,23 @@
+import "@fortawesome/fontawesome-free/css/all.css";
 import React from "react";
 
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content flex">
         <aside>
           <p>Made with ❤️ by alanalexxis</p>
+        </aside>
+        <aside className="ml-auto">
+          <a
+            href="https://github.com/alanalexxis"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-sm"
+          >
+            <i className="fab fa-github mr-2"></i>
+            GitHub
+          </a>
         </aside>
       </footer>
     </div>
