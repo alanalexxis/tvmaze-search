bcae62dce86cf055959053b8db31ba5b2954f16f
Alan Alexis <alanalexis@MacBook-Pro-de-Alan.local>
2024-01-28 00:39:49 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
